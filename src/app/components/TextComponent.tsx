45f248d6136a93dfb3c1b9f158c065a4e8432729
Abdullah Hisham Alsharif <abdullahalsharif86@gmail.com>
2024-09-22 18:42:44 +0300

    Initial commit

---
+++ src/app/components/TextComponent.tsx
@@ -0,0 +1,14 @@
+import { View,Text } from 'react-native' 
+import React from 'react'
+
+const TextComponent=() => {
+    return(
+        <View>
+            <Text> 
+                My teacher is Mr.mohammed musab khan
+            </Text>
+        </View>
+    )
+    
+}
+export default TextComponent
---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View , Text} from 'react-native'
+import React from 'react'
+import TextComponent from './components/TextComponent'
+
+const Index = () => {
+    return (
+        <View>
+            <TextComponent />
+        </View>
+    )
+}
+
+export default Index
