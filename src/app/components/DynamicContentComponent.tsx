112161115e2d7a96c4cda73ae767e87ea7b04853
Abdullah Hisham Alsharif <abdullahalsharif86@gmail.com>
2024-10-19 14:20:57 +0300

    i learened how to view the variable in the react native

--- src/app/components/DynamicContentComponent.tsx
+++ src/app/components/DynamicContentComponent.tsx
@@ -3,11 +3,15 @@ import React from 'react'
 
 const DynamicContentComponent = () => {
   const username ="Abdullah Hisham Alsharif"
+  let me = {name: "Abdullah Alsharif" ,age:15 ,grade: "grade nine" }
   return (
     <View>
       <Text>My Name Is {
         username
         }</Text>
+        <Text>
+          my name is  {me.name}, i am {me.age} years old,i am in {me.grade}
+        </Text>
     </View>
   )
 }
